bf0561c47811bb5d9e8febdb98239e9599d2a890
Darker <jmareda@seznam.cz>
2015-04-16 04:44:55 +0200

    Now testing resources using junit

--- test/cz/autoclient/TestResources.java
+++ test/cz/autoclient/TestResources.java
@@ -7,48 +7,46 @@
 package cz.autoclient;
 
 import cz.autoclient.GUI.ImageResources;
-import cz.autoclient.GUI.LazyLoadedRemoteImage;
-import cz.autoclient.autoclick.comvis.DebugDrawing;
 import java.awt.Image;
-import java.net.URL;
+import java.util.ArrayList;
 import javax.swing.ImageIcon;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+import org.junit.Test;
 /**
  *
  * @author Jakub
  */
+@RunWith(value = Parameterized.class)
 public class TestResources {
-   public static void main(String[] args) throws Exception
-   {
-     //Remember if everything was OK
-     boolean ok = true;
-     
-     System.out.println("Checking resources: \n");
-     for(ImageResources ir : ImageResources.values()) {
-       Image im = ir.getImage();
-       ImageIcon ico = ir.getIcon();
-       System.out.println("  ["+ir.name()+"]\n  - path: "+ir.getClasspath());
-       if(im==null || ico==null) {
-         System.out.println("    - ERROR!");
-         if(im==null)
-           System.out.println("      Image failed.");
-         if(ico==null)
-           System.out.println("      ImageIcon failed.");
-         ok = false;
-       }
-       else
-         System.out.println("    - OK.");
-     }
-     if(!ok) {
-       throw new Exception("There were broken resources! Remove them from enum or add them to .jar.");
-     }
-     
-     //Check remote image
-     /*LazyLoadedRemoteImage im = new LazyLoadedRemoteImage("test.png", new URL("http://cs.ceskestar.xx/images/inzerat/42_0.png"));
-     if(im.getImage()!=null)
-       DebugDrawing.displayImage(im.getImage());
-     else
-       System.out.println("Remote image not available.");*/
-     
-   }
+
+    //DO NOT MODIFY
+    ImageResources resource;
+
+    //DO NOT MODIFY
+    public TestResources(ImageResources resource) {
+        this.resource = resource;
+    }
+
+    //DO NOT MODIFY
+    @Parameterized.Parameters(name="{index}")
+    public static Iterable<Object[]> data() {
+      ArrayList<Object[]> params = new ArrayList<>();
+      for(ImageResources ir : ImageResources.values()) {
+        params.add(new Object[] {ir});
+      }
+      return params;
+    }
+    
+    @Test
+    public void testImage() {
+      Image im = resource.getImage();
+      assertNotNull("Image is null for "+resource.name()+"!", im);
+    }
+    @Test
+    public void testIcon() {
+      ImageIcon im = resource.getIcon();
+      assertNotNull("Icon is null for "+resource.name()+"!", im);
+    }
 }
