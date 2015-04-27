8d1f48bc90f01bf956aa0872d2aa33ec63dfcf1a
Darker <jmareda@seznam.cz>
2015-04-27 02:01:54 +0200

    Added interface for rectangles so that I can make enumerators of rectangles.

---
+++ src/cz/autoclient/autoclick/RectInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.autoclient.autoclick;
+
+/**
+ *
+ * @author Jakub
+ */
+public interface RectInterface {
+  public int getTop();
+  public int getLeft();
+  public int getWidth();
+  public int getHeight();
+  public RectInterface getCenter();
+}
