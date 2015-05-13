c4a6dc6d47ac9dbb1e7095d64adca8e261c172dc
Darker <jmareda@seznam.cz>
2015-05-14 01:30:38 +0200

    Added default regexp validator

--- src/cz/autoclient/autoclick/windows/WindowValidator.java
+++ src/cz/autoclient/autoclick/windows/WindowValidator.java
@@ -6,10 +6,24 @@
 
 package cz.autoclient.autoclick.windows;
 
+import cz.autoclient.autoclick.windows.Window;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Jakub
  */
 public interface WindowValidator {
   public boolean isValid(Window w);
+  
+  public static class TitlePatternWindowValidator implements WindowValidator {
+    private final Pattern pattern;
+    public TitlePatternWindowValidator(Pattern pattern) {
+      this.pattern = pattern;
+    }
+    @Override
+    public boolean isValid(Window w) {
+      return pattern.matcher(w.getTitle()).matches();
+    }
+  }
 }
