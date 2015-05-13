af7c22826b740d31f46a81a61b98e2a2d8245e01
Darker <jmareda@seznam.cz>
2015-05-14 01:22:34 +0200

    cleanup, reduced time between clicks

--- src/cz/autoclient/robots/LaunchBot.java
+++ src/cz/autoclient/robots/LaunchBot.java
@@ -31,7 +31,6 @@ public class LaunchBot extends Robot {
   /**
    * Remembers whether the last go() went with errors or not
    */
-  private boolean lastError = false;
   private boolean overSuccesful = false;
 
   /**
@@ -40,89 +39,70 @@ public class LaunchBot extends Robot {
    */
   @Override
   protected void go() throws InterruptedException {
-    //System.out.println("Start waiting for launch button.");
-    try {
-      while(true) {
-        //System.out.println("  - entered the loop");
-        if(Thread.interrupted())
-          throw new InterruptedException("Interrupted during main while(true).");
-        
-        //System.out.println("  - looking for the pixel");
-        if(WindowTools.checkPoint(window, PixelOffset.Patcher_Launch, 20)) {
-          Rect win_rect = window.getRect();
-          Rect pos = PixelOffset.Patcher_Launch.toRect(win_rect);
-          
-          /*BufferedImage im = window.screenshot();
-          DebugDrawing.drawPointOrRect(im, pos, Color.RED);
-          DebugDrawing.drawPointOrRect(im, PixelOffset.Patcher_SetServer.toRect(win_rect), Color.YELLOW);
-          DebugDrawing.displayImage(im);*/
-          
-          
-          window.everyChild(new WindowCallback() {
-            @Override
-            public void run(Window w) {
-              try {
-                w.slowClick(pos.left, pos.top, 80);
-                Thread.sleep(80);
-                try {
-                  while(WindowTools.checkPoint(window, PixelOffset.Patcher_Eula_Heading, 10)) {
-                    System.out.println("Accepting eula.");
-                    w.slowClick(PixelOffset.Patcher_Eula_Button.toRect(win_rect), 80);
-                    Thread.sleep(180);
-                  }
-                } catch (APIException ex) {
-                  Logger.getLogger(LaunchBot.class.getName()).log(Level.SEVERE, null, ex);
-                }
-              }
-              catch(InterruptedException e) {
-                t.interrupt();
+
+    while(true) {
+      //System.out.println("  - entered the loop");
+      if(Thread.interrupted())
+        throw new InterruptedException("Interrupted during main while(true).");
+
+      //System.out.println("  - looking for the pixel");
+      if(WindowTools.checkPoint(window, PixelOffset.Patcher_Launch, 20)) {
+        Rect win_rect = window.getRect();
+        Rect pos = PixelOffset.Patcher_Launch.toRect(win_rect);
+
+        /*BufferedImage im = window.screenshot();
+        DebugDrawing.drawPointOrRect(im, pos, Color.RED);
+        DebugDrawing.drawPointOrRect(im, PixelOffset.Patcher_SetServer.toRect(win_rect), Color.YELLOW);
+        DebugDrawing.displayImage(im);*/
+
+
+        window.everyChild(new WindowCallback() {
+          @Override
+          public void run(Window w) {
+            try {
+              w.slowClick(pos.left, pos.top, 80);
+              Thread.sleep(100);
+              while(WindowTools.checkPoint(window, PixelOffset.Patcher_Eula_Heading, 10)) {
+                System.out.println("Accepting eula.");
+                w.slowClick(PixelOffset.Patcher_Eula_Button.toRect(win_rect), 80);
+                Thread.sleep(180);
               }
             }
-          });//slowClick(pos.left, pos.top, 80);
-          
-          lastError = false;
-          overSuccesful = true;
-          //System.out.println("  - Clicked");
-          
-          break;
-        }
-        else {
-          //int[] diff = WindowTools.diffPoint(window, PixelOffset.Patcher_Launch.offset(0,0));
-          /*BufferedImage im = window.screenshot();
-          DebugDrawing.displayImage(im);*/
-          /*System.out.println("   Too much diff: ["+diff[0]+", "+diff[1]+", "+diff[2]+"]");
-          System.out.println("   Window size: "+window.getRect());
-          System.out.println("   Window title: "+window.getTitle());
-          System.out.println("   Window class name: "+MSWindow.getWindowClass(((MSWindow)window).hwnd));
-          System.out.println("   Window HWND : "+((MSWindow)window).hwnd);*/
-          //Get the window again
-          if(window.getRect().width<200) {
-            window = MSWindow.windowFromName(ConstData.patcher_window_title, false);
+            catch(InterruptedException e) {
+              t.interrupt();
+            }
           }
-        }
-        //System.out.println("  - Going to sleep.");
-        Thread.sleep(1000);
-      }
-    } catch(APIException e) {
-      lastError = true;
-      //Recover if the window is still valid
-      if(window.isValid() && !lastError) {
-        //System.out.println("WINDOW ERROR!");
-        go();
+        });//slowClick(pos.left, pos.top, 80);
+
+        //System.out.println("  - Clicked");
+
+        break;
       }
       else {
-        //System.out.println("WINDOW ERROR! GAME OVAR!");
-        return;
+        //int[] diff = WindowTools.diffPoint(window, PixelOffset.Patcher_Launch.offset(0,0));
+        /*BufferedImage im = window.screenshot();
+        DebugDrawing.displayImage(im);*/
+        /*System.out.println("   Too much diff: ["+diff[0]+", "+diff[1]+", "+diff[2]+"]");
+        System.out.println("   Window size: "+window.getRect());
+        System.out.println("   Window title: "+window.getTitle());
+        System.out.println("   Window class name: "+MSWindow.getWindowClass(((MSWindow)window).hwnd));
+        System.out.println("   Window HWND : "+((MSWindow)window).hwnd);*/
+        //Get the window again
+        if(window.getRect().width<200) {
+          window = MSWindow.windowFromName(ConstData.patcher_window_title, false);
+        }
       }
-      //else
-        //return;
+      //System.out.println("  - Going to sleep.");
+      Thread.sleep(1000);
     }
+    overSuccesful = true;
+
     //System.out.println("WUT!");
   }
   
   @Override
   protected void init() {
-    lastError = overSuccesful = false;
+    overSuccesful = false;
   }
   /**
    * Additionally to valid window, LaunchBot will require certain delay between
