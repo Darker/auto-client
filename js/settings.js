4b6c858066bae0cfa1c38a0538ba663e5f752bb2
Darker <jmareda@seznam.cz>
2016-05-08 14:28:59 +0200

    Settings from localStorage now override global settings.

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,4 @@
+// localStorage["autoclient-settings"] = JSON.stringify({debug: true});
 Object.defineProperty(Object.prototype, "addkeys", {
    value: function(other, replace, create) {
       if(typeof create=="undefined")
@@ -14,18 +15,23 @@ Object.defineProperty(Object.prototype, "addkeys", {
 });
 var SETTINGS = {debug: false};
 
+// localStorage["autoclient-settings"] = JSON.stringify({debug: true});
 if(localStorage["autoclient-settings"]) {
   try {
     var saved_settings = JSON.parse(localStorage["autoclient-settings"]);
-    SETTINGS.addkeys(saved_settings, true, false);
+    SETTINGS.addkeys(saved_settings, true, true);
   }
   catch(e) {
     console.warn("Error parsing localStorage settings: ",e);
   }
 }
+/**
+ * Settings from URL can only overwrite, not add. That prevents XSS hopefully.
+**/ 
 if(location.hash.length>1) {
   try {
     var user_settings = JSON.parse(decodeURIComponent(location.hash.substr(1)));
+    console.log("User settings: ", user_settings);
     SETTINGS.addkeys(user_settings, true, false);
   }
   catch(e) {
