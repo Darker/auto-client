14d86b02ee9f437b6a0c941ddfb3703e2e4e190c
Jakub Mareda <mareda@techsys.cz>
2016-05-09 14:00:50 +0200

    added error reporting in ga and upgraded settings

--- js/googleanalytics.js
+++ js/googleanalytics.js
@@ -1,4 +1,4 @@
-if(location.href.indexOf("127.0.0.1")==-1 && !SETTINGS.debug) {
+if(location.href.indexOf("127.0.0.1")==-1 && (typeof SETTINGS=="undefined" || !SETTINGS.debug)) {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -7,10 +7,37 @@ if(location.href.indexOf("127.0.0.1")==-1 && !SETTINGS.debug) {
   ga('create', 'UA-77379209-1', 'auto');
   ga('send', 'pageview');
   
-  $("a").click(function() {
-    ga('send', 'event', 'outbound', 'click', this.href, {
-         'transport': 'beacon',
-         //'hitCallback': function(){document.location = url;}
-       });
+  try {
+    var links = document.getElementsByTagName("a");
+    var link_callback = function() {
+      ga('send', 'event', 'outbound', 'click', this.href, {
+           'transport': 'beacon',
+           //'hitCallback': function(){document.location = url;}
+         });
+    };
+    for(var i=0,l=links.length; i<l; i++) {
+      links[i].addEventListener(link_callback);
+    } 
+  }
+  catch(e) {
+    console.warn("Failed to add link callbacks.");
+  }
+}
+try {
+  window.addEventListener("error", function(e) {
+    var file = e.filename.substr(e.filename.lastIndexOf("/")+1);
+    if(file.length==0)
+      file = e.filename;
+    var msg = file+":"+e.lineno+":"+e.colno+" \""+e.message+"\" Browser: "+navigator.userAgent;
+    
+    if(typeof ga=="function") {
+      ga('send', 'event', 'jserror', 'click', msg, {'transport': 'beacon'});
+    }
+    else {
+      console.error(msg);
+    }
   });
+}
+catch(e) {
+  console.warn("Failed to add error callback.");
 }
--- js/settings.js
+++ js/settings.js
@@ -1,45 +1,118 @@
-// localStorage["autoclient-settings"] = JSON.stringify({debug: true});
-Object.defineProperty(Object.prototype, "addkeys", {
-   value: function(other, replace, create) {
-      if(typeof create=="undefined")
-        create = true;
-      if(typeof replace=="undefined")
-        replace = true;
-      for(var i in other) {
-        var defined = this[i]!="undefined";
-        if((defined && replace) || (!defined && create))
-          this[i] = other[i];
-      }
-    },
-    enumerable: false
-});
-var SETTINGS = {
+function Settings(superSettings, data) {
+  this.parent = superSettings;
+  if(superSettings instanceof Settings) {
+    this.makeGetters(superSettings.data);
+  }
+  if(typeof data=="object") {
+    this.data = data;
+    this.makeGetters(data);
+  }
+  else
+    this.data = {};       
+}
+Settings.prototype.constructor = Settings;
+Settings.prototype.set = function(name, value) {
+  this.makeGetter(name);
+  return this.data[name] = value;
+}
+
+Settings.prototype.get = function(name) {
+  if(typeof this.data[name]!="undefined")
+    return this.data[name];
+  else if(this.parent instanceof Settings)
+    return this.parent.get(name);
+  else
+    return undefined;
+}
+Settings.prototype.del = function(name) {
+  delete this.data[name];
+}
+Settings.prototype.delAll = function() {
+  this.data = {};
+}
+
+Settings.prototype.makeGetter = function(name) {
+  if(!this.hasOwnProperty(name)) {
+    if(typeof this.__proto__[name]=="undefined") {
+      Object.defineProperty(this, name, {
+        get: function() {
+          console.log("Getter "+this.constructor.name+"::get(",name,") with ", this, " returns: ", this.get(name));
+          return this.get(name);
+        }, 
+        set: function(value) {
+          return this.set(name, value);
+        } 
+      });
+    }
+    else {
+      console.warn("Attempt to override prototype property "+name+" in settings.");
+    }
+  }
+} 
+Settings.prototype.makeGetters = function(obj) {
+  for(var i in obj) {
+    if(obj.hasOwnProperty(i)) {
+      this.makeGetter(i);
+    }
+  }
+}
+
+
+function SynchronizedSettings(superSettings, name, settings_names) {
+  // super constructor
+  Settings.call(this, superSettings);
+  
+  this.name = name;
+  this.reloadSettings();
+  window.addEventListener("storage", this.reloadSettings = this.reloadSettings.bind(this));
+}
+SynchronizedSettings.prototype = Object.create(Settings.prototype);
+SynchronizedSettings.prototype.constructor = SynchronizedSettings;
+
+SynchronizedSettings.prototype.reloadSettings = function() {
+  if(localStorage[this.name]) {
+    try {
+      this.data = JSON.parse(localStorage[this.name]);
+      console.log("User settings: ", this.data);
+      this.makeGetters(this.data);
+    }
+    catch(e) {
+      console.warn("Error parsing localStorage settings: ",e);
+    }
+  }
+}
+SynchronizedSettings.prototype.saveSettings = function() {
+  localStorage[this.name] = JSON.stringify(this.data);
+}
+
+SynchronizedSettings.prototype.set = function(name, value) {
+  this.data[name] = value;
+  this.makeGetter(name);
+  this.saveSettings();
+  return value;
+}
+Settings.prototype.del = function(name) {
+  delete this.data[name];
+  this.saveSettings();
+}
+Settings.prototype.delAll = function() {
+  this.data = {};
+  this.saveSettings();
+}
+
+
+var DEFAULT_SETTINGS = new Settings(null, {
     debug: false,
     auto_download: true,
     gh_cache_max_age: 60*60*1000
-};
-var LOCAL_SETTINGS = {};
-var LOCAL_SETTINGS_NAME = "autoclient-settings";
-// localStorage["autoclient-settings"] = JSON.stringify({debug: true});
-if(localStorage[LOCAL_SETTINGS_NAME]) {
-  try {
-    var LOCAL_SETTINGS = JSON.parse(localStorage[LOCAL_SETTINGS_NAME]);
-    SETTINGS.addkeys(LOCAL_SETTINGS, true, true);
-  }
-  catch(e) {
-    console.warn("Error parsing localStorage settings: ",e);
-  }
-}
-function changeLocalSetting(name, value) {
-  LOCAL_SETTINGS[name] = value;
-  SETTINGS[name] = value;
-  localStorage[LOCAL_SETTINGS_NAME] = JSON.stringify(LOCAL_SETTINGS);
-}
+});
+var LOCAL_SETTINGS = new SynchronizedSettings(DEFAULT_SETTINGS, "autoclient-settings");
+var SETTINGS = LOCAL_SETTINGS;
 
 /**
  * Settings from URL can only overwrite, not add. That prevents XSS hopefully.
 **/ 
-if(location.hash.length>1) {
+/*if(location.hash.length>1) {
   try {
     var user_settings = JSON.parse(decodeURIComponent(location.hash.substr(1)));
     console.log("User settings: ", user_settings);
@@ -48,4 +121,4 @@ if(location.hash.length>1) {
   catch(e) {
     console.warn("Error parsing url settings: ",e);
   }
-}
+}    */
